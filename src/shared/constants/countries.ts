f64b20c4cd2b19b505e198bed01081d2a700190f
Carlos Zavala <carloszavala@Carloss-MacBook-Pro.local>
2026-08-21 14:52:11 -0500

    Adding country to register form

--- src/shared/constants/countries.ts
+++ src/shared/constants/countries.ts
@@ -26,3 +26,132 @@ export const COUNTRY_OPTIONS = [
   { value: "in", label: "India" },
   { value: "all", label: "All countries" },
 ] as const;
+
+/**
+ * States/provinces/regions by country code.
+ * Mirrors swift-slate/src/lib/countries.ts (STATES_BY_COUNTRY).
+ */
+export const STATES_BY_COUNTRY: Record<string, string[]> = {
+  us: [
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+    "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+    "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+    "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+    "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",
+  ],
+  ca: [
+    "Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador",
+    "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island",
+    "Quebec", "Saskatchewan", "Yukon",
+  ],
+  mx: [
+    "Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Chiapas",
+    "Chihuahua", "Ciudad de México", "Coahuila", "Colima", "Durango", "Guanajuato",
+    "Guerrero", "Hidalgo", "Jalisco", "México", "Michoacán", "Morelos", "Nayarit",
+    "Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí",
+    "Sinaloa", "Sonora", "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatán", "Zacatecas",
+  ],
+  gb: [
+    "East Midlands", "East of England", "London", "North East England", "North West England",
+    "Northern Ireland", "Scotland", "South East England", "South West England", "Wales",
+    "West Midlands", "Yorkshire and the Humber",
+  ],
+  au: [
+    "Australian Capital Territory", "New South Wales", "Northern Territory", "Queensland",
+    "South Australia", "Tasmania", "Victoria", "Western Australia",
+  ],
+  de: [
+    "Baden-Württemberg", "Bavaria", "Berlin", "Brandenburg", "Bremen", "Hamburg", "Hesse",
+    "Lower Saxony", "Mecklenburg-Vorpommern", "North Rhine-Westphalia", "Rhineland-Palatinate",
+    "Saarland", "Saxony", "Saxony-Anhalt", "Schleswig-Holstein", "Thuringia",
+  ],
+  fr: [
+    "Auvergne-Rhône-Alpes", "Bourgogne-Franche-Comté", "Brittany", "Centre-Val de Loire",
+    "Corsica", "Grand Est", "Guadeloupe", "Guyane", "Hauts-de-France", "Île-de-France",
+    "La Réunion", "Martinique", "Mayotte", "Normandy", "Nouvelle-Aquitaine",
+    "Occitanie", "Pays de la Loire", "Provence-Alpes-Côte d'Azur",
+  ],
+  es: [
+    "Andalusia", "Aragon", "Asturias", "Balearic Islands", "Basque Country", "Canary Islands",
+    "Cantabria", "Castilla-La Mancha", "Castilla y León", "Catalonia", "Ceuta",
+    "Extremadura", "Galicia", "La Rioja", "Madrid", "Melilla", "Murcia", "Navarre", "Valencia",
+  ],
+  it: [
+    "Abruzzo", "Aosta Valley", "Apulia", "Basilicata", "Calabria", "Campania",
+    "Emilia-Romagna", "Friuli-Venezia Giulia", "Lazio", "Liguria", "Lombardy", "Marche",
+    "Molise", "Piedmont", "Sardinia", "Sicily", "Trentino-South Tyrol", "Tuscany",
+    "Umbria", "Veneto",
+  ],
+  nl: [
+    "Drenthe", "Flevoland", "Friesland", "Gelderland", "Groningen", "Limburg",
+    "North Brabant", "North Holland", "Overijssel", "South Holland", "Utrecht", "Zeeland",
+  ],
+  br: [
+    "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+    "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul",
+    "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
+    "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima",
+    "Santa Catarina", "São Paulo", "Sergipe", "Tocantins",
+  ],
+  ar: [
+    "Buenos Aires", "Buenos Aires City (CABA)", "Catamarca", "Chaco", "Chubut", "Córdoba",
+    "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza",
+    "Misiones", "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz",
+    "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucumán",
+  ],
+  co: [
+    "Amazonas", "Antioquia", "Arauca", "Atlántico", "Bogotá D.C.", "Bolívar", "Boyacá",
+    "Caldas", "Caquetá", "Casanare", "Cauca", "Cesar", "Chocó", "Córdoba", "Cundinamarca",
+    "Guainía", "Guaviare", "Huila", "La Guajira", "Magdalena", "Meta", "Nariño",
+    "Norte de Santander", "Putumayo", "Quindío", "Risaralda", "San Andrés y Providencia",
+    "Santander", "Sucre", "Tolima", "Valle del Cauca", "Vaupés", "Vichada",
+  ],
+  cl: [
+    "Antofagasta", "Araucanía", "Arica y Parinacota", "Atacama", "Aysén", "Biobío",
+    "Coquimbo", "Los Lagos", "Los Ríos", "Magallanes", "Maule", "Metropolitana de Santiago",
+    "Ñuble", "O'Higgins", "Tarapacá", "Valparaíso",
+  ],
+  pe: [
+    "Amazonas", "Áncash", "Apurímac", "Arequipa", "Ayacucho", "Cajamarca", "Callao",
+    "Cusco", "Huancavelica", "Huánuco", "Ica", "Junín", "La Libertad", "Lambayeque",
+    "Lima", "Loreto", "Madre de Dios", "Moquegua", "Pasco", "Piura", "Puno",
+    "San Martín", "Tacna", "Tumbes", "Ucayali",
+  ],
+  ec: [
+    "Azuay", "Bolívar", "Cañar", "Carchi", "Chimborazo", "Cotopaxi", "El Oro",
+    "Esmeraldas", "Galápagos", "Guayas", "Imbabura", "Loja", "Los Ríos", "Manabí",
+    "Morona Santiago", "Napo", "Orellana", "Pastaza", "Pichincha", "Santa Elena",
+    "Santo Domingo de los Tsáchilas", "Sucumbíos", "Tungurahua", "Zamora-Chinchipe",
+  ],
+  ie: [
+    "Carlow", "Cavan", "Clare", "Cork", "Donegal", "Dublin", "Galway", "Kerry",
+    "Kildare", "Kilkenny", "Laois", "Leitrim", "Limerick", "Longford", "Louth",
+    "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon", "Sligo", "Tipperary",
+    "Waterford", "Westmeath", "Wexford", "Wicklow",
+  ],
+  nz: [
+    "Auckland", "Bay of Plenty", "Canterbury", "Gisborne", "Hawke's Bay",
+    "Manawatu-Whanganui", "Marlborough", "Nelson", "Northland", "Otago",
+    "Southland", "Taranaki", "Tasman", "Waikato", "Wellington", "West Coast",
+  ],
+  jp: [
+    "Aichi", "Akita", "Aomori", "Chiba", "Ehime", "Fukui", "Fukuoka", "Fukushima",
+    "Gifu", "Gunma", "Hiroshima", "Hokkaido", "Hyogo", "Ibaraki", "Ishikawa", "Iwate",
+    "Kagawa", "Kagoshima", "Kanagawa", "Kochi", "Kumamoto", "Kyoto", "Mie", "Miyagi",
+    "Miyazaki", "Nagano", "Nagasaki", "Nara", "Niigata", "Oita", "Okayama", "Okinawa",
+    "Osaka", "Saga", "Saitama", "Shiga", "Shimane", "Shizuoka", "Tochigi", "Tokushima",
+    "Tokyo", "Tottori", "Toyama", "Wakayama", "Yamagata", "Yamaguchi", "Yamanashi",
+  ],
+  in: [
+    "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa",
+    "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala",
+    "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland",
+    "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura",
+    "Uttar Pradesh", "Uttarakhand", "West Bengal",
+    "Andaman and Nicobar Islands", "Chandigarh", "Dadra and Nagar Haveli and Daman and Diu",
+    "Delhi", "Jammu and Kashmir", "Ladakh", "Lakshadweep", "Puducherry",
+  ],
+};
+
